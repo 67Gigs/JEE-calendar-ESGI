1309a26cc41039e7bf6fef2e208d7842c5def80b
noureddine.bensadok@ouidou.fr <noureddine.bensadok@ouidou.fr>
2025-04-02 08:18:27 +0200

    first commit

---
+++ src/test/java/fr/esgi/noureddine/ben_sadok/calendrier/CalendrierApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.noureddine.ben_sadok.calendrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
